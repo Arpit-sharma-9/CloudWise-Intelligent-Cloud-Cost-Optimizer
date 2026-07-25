13d4b0d32e756b77e3b2ad20b099aef5e9d952aa
Arpit Sharma <Galgotiasuniversity79@gmail.com>
2026-07-25 10:27:46 +0530

    Initial Spring Boot application setup

--- src/main/java/com/cloudwise/CloudWiseApplication.java
+++ src/main/java/com/cloudwise/CloudWiseApplication.java
@@ -1,1 +1,12 @@
+package com.cloudwise;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWiseApplication.class, args);
+    }
+}
