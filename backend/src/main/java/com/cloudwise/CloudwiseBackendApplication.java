36e7ffaa6241484a0b568e5c0023099266d6ddb9
Arpit Sharma <Galgotiasuniversity79@gmail.com>
2026-07-28 12:07:05 +0530

    Add main application class for Spring Boot

---
+++ backend/src/main/java/com/cloudwise/CloudwiseBackendApplication.java
@@ -0,0 +1,12 @@
+package com.cloudwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudwiseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudwiseBackendApplication.class, args);
+    }
+}
